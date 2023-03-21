62dcc35f44d00c1d4f04b7830031dc8cd5f2f4e7
Abhinav Sharma <abhinav291sharma@gmail.com>
2023-03-21 20:35:55 +0530

    Modifications made

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,9 +1,24 @@
 import React from "react";
+import linked from "./images/link.jpg"
+import github from "./images/github.jpg"
 
 export const Contact = () => {
     return(
-        <div>
-
+        <div class="container">
+            <h1 class="display-4">To Reach Me:</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <img src={linked} class="img-thumbnail" alt="..." height={175} width={175}></img>
+                    </div>
+                    <div class="col-sm">
+                        <img src={github} class="img-thumbnail" alt="..." height={175} width={175}></img>
+                    </div>
+                </div>
+            </div>
+            <blockquote class="blockquote">
+                <p class="mb-0">Email: abhinav291sharma@gmail.com</p>
+            </blockquote>
         </div>
     );
 }
--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -2,8 +2,79 @@ import React from "react";
 
 export const Project = () => {
     return(
-        <div>
-
+        <div class="list-group">
+            <a href="https://github.com/Abhinav5050649/bankTr" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">bankTr</h5>
+                <small>A Bank Management System</small>
+                </div>
+                <p class="mb-1">A MERN Web App which implements a Bank Management System</p>
+                <small>Tech Stack: MongoDB, ExpressJs, ReactJs, NodeJs</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/TreePaths" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">TreePaths</h5>
+                <small>A Command-Line Interface Based Game</small>
+                </div>
+                <p class="mb-1">A Text-Based CLI RPG, utilising the concepts of Tree DS</p>
+                <small>Tech Stack: C</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/MERN_Init_Automater" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">MERN_Init_Automater</h5>
+                <small>Automates initialisation of MERN App</small>
+                </div>
+                <p class="mb-1">A PowerShell Script which automates the initialisation of MERN Web App</p>
+                <small>Tech Stack: PowerShell</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/DiaryApp" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">DiaryApp</h5>
+                <small>A Web-Based Diary App</small>
+                </div>
+                <p class="mb-1">A MERN Based Web App which works as an Online Diary</p>
+                <small>Tech Stack: MongoDB, ExpressJs, ReactJs, NodeJs</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/Py_Git_Auto" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Py_Git_Auto</h5>
+                <small>Automation of Basic Git Commands</small>
+                </div>
+                <p class="mb-1">This is a Python Script which helps automate the basic Git Commands like 'add', 'commit</p>
+                <small>Tech Stack: Python</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/Delimiter" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Delimiter</h5>
+                <small>A C++ Based Delimiter Checker</small>
+                </div>
+                <p class="mb-1">A C++ Implementation of Basic Delimiter Checkers implemented by IDE's</p>
+                <small>Tech Stack: C++</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/Encryptor_Decryptor" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Encryptor_Decryptor</h5>
+                <small>A CLI-Based Text Encryptor-Decryptor</small>
+                </div>
+                <p class="mb-1">A Python program to encrypt or decrypt text utilising Morse Code, RSA, SHA256 and Fernet for this purpose</p>
+                <small>Tech Stack: Python</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/ActivitiesTracker" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">ActivitiesTracker</h5>
+                <small>A Simple MERN App</small>
+                </div>
+                <p class="mb-1">A basic MERN Project, with help of which, user can keep track of his/her activites</p>
+                <small>Tech Stack: MongoDB, ExpressJs, ReactJs, NodeJs</small>
+            </a>
+            <a href="https://github.com/Abhinav5050649/CreditCardValidator" class="list-group-item list-group-item-action active">
+                <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">CreditCardValidator</h5>
+                <small>Validates the authenticity of Credit Card</small>
+                </div>
+                <p class="mb-1">A C++ program which verifies the authenticity of a Credit Card based on it's Credit Card Number</p>
+                <small>Tech Stack: C++</small>
+            </a>
         </div>
     );
 }
